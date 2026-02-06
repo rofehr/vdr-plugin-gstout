a2df1e5430d6642007aa9abb34c8926e4b47dc88
rofehr <roland@diefehrs.de>
2026-02-06 10:24:51 +0100

    changes

--- gstoutput.c
+++ gstoutput.c
@@ -586,7 +586,8 @@ void cGstVideoOutput::NeedDataCallback(GstElement *source, guint size, gpointer 
   int available = self->buffer->Available();
   if (available > 0) {
     uchar *readData;
-    int count = self->buffer->Get(readData, available);
+    int count = 0;
+	readData = self->buffer->Get(count);
     if (count > 0) {
       GstBuffer *gstBuffer = gst_buffer_new_allocate(NULL, count, NULL);
       GstMapInfo map;
